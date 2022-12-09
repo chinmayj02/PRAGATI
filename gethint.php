4eb54c600ca65a3235d1290327aac430392f6e55
SkylaBarreto <2014053@dbcegoa.ac.in>
2022-12-09 17:13:03 +0530

    search bar

---
+++ gethint.php
@@ -0,0 +1,40 @@
+<?php
+// Array with names
+$a[] = "Apple";
+$a[] = "Banana";
+$a[] = "Pears";
+$a[] = "Pumpkin";
+$a[] = "Brinjals";
+$a[] = "Bell peppers";
+$a[] = "Chillies";
+$a[] = "Artwork";
+$a[] = "Basket";
+$a[] = "Chickoo";
+$a[] = "Papaya";
+$a[] = "Leafy vegetables";
+$a[] = "Cashew";
+
+
+// get the q parameter from URL
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// lookup all hints from array if $q is different from ""
+if ($q !== "") {
+  $q = strtolower($q);
+  $len=strlen($q);
+  foreach($a as $name) {
+    if (stristr($q, substr($name, 0, $len))) {
+      if ($hint === "") {
+        $hint = $name;
+      } else {
+        $hint .= ", $name";
+      }
+    }
+  }
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
